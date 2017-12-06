5380e851c809838ecb93dcd7cceef6e1bdbde447
mambax7 <mambax7@gmail.com>
2017-12-06 07:27:33 -0500

    SQL Formatting

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,25 +1,27 @@
 CREATE TABLE `rssfit_misc` (
-  `misc_id` smallint(5) unsigned NOT NULL auto_increment,
-  `misc_category` varchar(30) NOT NULL default '',
-  `misc_title` varchar(255) NOT NULL default '',
-  `misc_content` text NOT NULL,
-  `misc_setting` text NOT NULL,
-  PRIMARY KEY  (`misc_id`)
-) ENGINE=MyISAM;
+  `misc_id`       SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `misc_category` VARCHAR(30)          NOT NULL DEFAULT '',
+  `misc_title`    VARCHAR(255)         NOT NULL DEFAULT '',
+  `misc_content`  TEXT                 NOT NULL,
+  `misc_setting`  TEXT                 NOT NULL,
+  PRIMARY KEY (`misc_id`)
+)
+  ENGINE = MyISAM;
 
 CREATE TABLE `rssfit_plugins` (
-  `rssf_conf_id` int(5) unsigned NOT NULL auto_increment,
-  `rssf_filename` varchar(50) NOT NULL default '',
-  `rssf_activated` tinyint(1) NOT NULL default '0',
-  `rssf_grab` tinyint(2) NOT NULL default '0',
-  `rssf_order` tinyint(2) NOT NULL default '0',
-  `subfeed` tinyint(1) NOT NULL default '0',
-  `sub_entries` char(2) NOT NULL default '',
-  `sub_link` varchar(255) NOT NULL default '',
-  `sub_title` varchar(255) NOT NULL default '',
-  `sub_desc` varchar(255) NOT NULL default '',
-  `img_url` varchar(255) NOT NULL default '',
-  `img_link` varchar(255) NOT NULL default '',
-  `img_title` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`rssf_conf_id`)
-) ENGINE=MyISAM;
+  `rssf_conf_id`   INT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `rssf_filename`  VARCHAR(50)     NOT NULL DEFAULT '',
+  `rssf_activated` TINYINT(1)      NOT NULL DEFAULT '0',
+  `rssf_grab`      TINYINT(2)      NOT NULL DEFAULT '0',
+  `rssf_order`     TINYINT(2)      NOT NULL DEFAULT '0',
+  `subfeed`        TINYINT(1)      NOT NULL DEFAULT '0',
+  `sub_entries`    CHAR(2)         NOT NULL DEFAULT '',
+  `sub_link`       VARCHAR(255)    NOT NULL DEFAULT '',
+  `sub_title`      VARCHAR(255)    NOT NULL DEFAULT '',
+  `sub_desc`       VARCHAR(255)    NOT NULL DEFAULT '',
+  `img_url`        VARCHAR(255)    NOT NULL DEFAULT '',
+  `img_link`       VARCHAR(255)    NOT NULL DEFAULT '',
+  `img_title`      VARCHAR(255)    NOT NULL DEFAULT '',
+  PRIMARY KEY (`rssf_conf_id`)
+)
+  ENGINE = MyISAM;
